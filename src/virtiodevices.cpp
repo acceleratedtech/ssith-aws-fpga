3c9430ca5515870128fdee23709dc4cc6c9fe505
Jamey Hicks <jamey.hicks@accelerated.tech>
2020-04-13 15:03:21 +0000

    fill in necessary fields of virtio_bus and iniitialize IRQ

--- src/virtiodevices.cpp
+++ src/virtiodevices.cpp
@@ -8,16 +8,23 @@ extern "C" {
 
 #include "virtiodevices.h"
 
+void awsp2_set_irq(void *opaque, int irq_num, int level)
+{
+    fprintf(stderr, "%s: irq_num=%d level=%d\n", __FUNCTION__, irq_num, level);
+}
 
 VirtioDevices::VirtioDevices() {
-  console = console_init(1);
-  mem_map = phys_mem_map_init();
-  virtio_bus = (VIRTIOBusDef *)mallocz(sizeof(*virtio_bus));
-  virtio_bus->mem_map = mem_map;
-  virtio_bus->addr = 0x40000000;
+    console = console_init(1);
+    mem_map = phys_mem_map_init();
+    IRQSignal *irq = (IRQSignal *)mallocz(4 * sizeof(IRQSignal));
+    virtio_bus = (VIRTIOBusDef *)mallocz(sizeof(*virtio_bus));
+    virtio_bus->mem_map = mem_map;
+    virtio_bus->addr = 0x40000000;
+    virtio_bus->irq = irq;
+    irq_init(irq, awsp2_set_irq, (void *)22, 0);
 
-  virtio_console = virtio_console_init(virtio_bus, console);
-  fprintf(stderr, "virtio_console=%08p\n", virtio_console);
+    virtio_console = virtio_console_init(virtio_bus, console);
+    fprintf(stderr, "virtio_console=%p\n", virtio_console);
 }
 
 VirtioDevices::~VirtioDevices() {
@@ -25,5 +32,5 @@ VirtioDevices::~VirtioDevices() {
 
 PhysMemoryRange *VirtioDevices::get_phys_mem_range(uint64_t paddr)
 {
-  return ::get_phys_mem_range(mem_map, paddr);
+    return ::get_phys_mem_range(mem_map, paddr);
 }
