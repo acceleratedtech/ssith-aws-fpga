fbad27b3c2a8e97a3aeb57ddfc56c99676066714
Jessica Clarke <jrtc27@jrtc27.com>
2020-06-20 08:16:25 +0100

    Change place_directive to AltSpreadLogic_low
    This makes it less aggressive than the CONGESTION stratey it was copied
    from. Although we are congested, we also have long paths, so spreading
    the logic out too much creates lots of work for the router. This change
    seems to be a happier middle-ground between timing and congestion
    strategies.

--- hw/scripts/strategy_OVERRIDES.tcl
+++ hw/scripts/strategy_OVERRIDES.tcl
@@ -1,6 +1,6 @@
 set synth_options   "-no_lc -shreg_min_size 10 -control_set_opt_threshold 16 $synth_uram_option"
 set synth_directive "AlternateRoutability"
 
-set place_directive "AltSpreadLogic_medium"
+set place_directive "AltSpreadLogic_low"
 
 set route_directive "AggressiveExplore"
