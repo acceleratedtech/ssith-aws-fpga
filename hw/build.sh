b5829c81308acba822808413f280556bbfe5f0f5
Jamey Hicks <jamey.hicks@cmtelematics.com>
2020-12-09 20:22:34 +0000

    improved hw/build.sh

--- hw/build.sh
+++ hw/build.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
 
+function fatal {
+    echo "FATAL: $1"
+    exit 1
+}
+
+aws s3 ls s3://aws-fpga > /dev/null || fatal "No access to S3 bucket aws-fpga. Check aws credentials or create the bucket"
+
 . aws-fpga/hdk_setup.sh
 export BLUESPECDIR=/opt/bluespec/lib
 (cd src_AWS_P2_MIT; make gen.awsf1)
-docker run -it --env BLUESPECDIR=$BLUESPECDIR -v $PWD:$PWD jameyhicks/connectal:latest make -j8 -C $PWD/src_AWS_P2_MIT/awsf1 verilog
+export BLUESPECDIR=$PWD/bluespec/lib
+export CONNECTALDIR=$PWD/connectal
+sudo docker run --rm -it --env BLUESPECDIR=$BLUESPECDIR -v $PWD:$PWD jameyhicks/connectal:latest cp -r /opt/bluespec $PWD
+sudo docker run --rm -it --env BLUESPECDIR=$BLUESPECDIR -v $PWD:$PWD jameyhicks/connectal:latest make -j8 -C $PWD/src_AWS_P2_MIT/awsf1 verilog
 cd src_AWS_P2_MIT/awsf1
-CONNECTALDIR=/home/centos/ssith-aws-fpga/hw/connectal /home/centos/ssith-aws-fpga/hw/connectal/scripts/aws/build.sh -strategy TIMING -clock_recipe_a A1 -clock_recipe_b B5
+/home/centos/ssith-aws-fpga/hw/connectal/scripts/aws/build.sh -strategy TIMING -clock_recipe_a A1 -clock_recipe_b B5
 
 
